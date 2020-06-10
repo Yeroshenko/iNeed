49d43169341a18f0eedaa0066b9ab3349e3a7315
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-06-10 13:41:43 +0300

    feat: created useClickOutside hook and used inside TooCreator

--- src/components/TodoCreator.jsx
+++ src/components/TodoCreator.jsx
@@ -1,55 +1,65 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import cn from 'classnames'
 import { Form, Input, Button } from 'antd'
 
 import { max } from 'utils'
 import { PlusIcon } from 'icons'
+import { useOutsideClick } from 'hooks'
 import 'styles/components/TodoCreator.sass'
 
-const TodoCreator = ({ featching, createMode, onSubmit, toggleCreateMode }) => (
-  <div className={cn('todo-creator', { 'todo-creator--created': createMode })}>
-    {!createMode && (
-      <div className='todo-creator__trigger' onClick={toggleCreateMode}>
-        <PlusIcon />
-        <span className='todo-creator__trigger-text'>Новая задача</span>
-      </div>
-    )}
+const TodoCreator = ({ featching, createMode, onSubmit, toggleCreateMode }) => {
+  const todoCreatorRef = useRef()
 
-    {createMode && (
-      <Form
-        name='basic'
-        size='large'
-        className='todo-creator__form'
-        onFinish={onSubmit}
-      >
-        <Form.Item
-          name='title'
-          className='todo-creator__input'
-          rules={[max(512, 'Максимальная длина задачи 512 символов')]}
-        >
-          <Input autoFocus placeholder='Текст задачи' />
-        </Form.Item>
+  useOutsideClick(todoCreatorRef, toggleCreateMode, createMode)
 
-        <div className='todo-creator__buttons'>
-          <Button
-            className='todo-creator__button'
-            type='primary'
-            loading={featching}
-            htmlType='submit'
-          >
-            {featching ? 'Добавление задачи' : 'Добавить задачу'}
-          </Button>
-          <Button
-            className='todo-creator__button'
-            onClick={toggleCreateMode}
-            htmlType='reset'
-          >
-            Отменить
-          </Button>
+  return (
+    <div
+      className={cn('todo-creator', { 'todo-creator--created': createMode })}
+      ref={todoCreatorRef}
+    >
+      {!createMode && (
+        <div className='todo-creator__trigger' onClick={toggleCreateMode}>
+          <PlusIcon />
+          <span className='todo-creator__trigger-text'>Новая задача</span>
         </div>
-      </Form>
-    )}
-  </div>
-)
+      )}
+
+      {createMode && (
+        <Form
+          name='basic'
+          size='large'
+          className='todo-creator__form'
+          onFinish={onSubmit}
+        >
+          <Form.Item
+            name='title'
+            className='todo-creator__input'
+            rules={[max(512, 'Максимальная длина задачи 512 символов')]}
+          >
+            <Input autoFocus placeholder='Текст задачи' />
+          </Form.Item>
+
+          <div className='todo-creator__buttons'>
+            <Button
+              className='todo-creator__button'
+              type='primary'
+              loading={featching}
+              htmlType='submit'
+            >
+              {featching ? 'Добавление задачи' : 'Добавить задачу'}
+            </Button>
+            <Button
+              className='todo-creator__button'
+              onClick={toggleCreateMode}
+              htmlType='reset'
+            >
+              Отменить
+            </Button>
+          </div>
+        </Form>
+      )}
+    </div>
+  )
+}
 
 export default TodoCreator
---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from 'react'
+
+export const useOutsideClick = (ref, callback, when) => {
+  const savedCallback = useRef(callback)
+
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
+  const handler = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      savedCallback.current()
+    }
+  }
+
+  useEffect(() => {
+    if (when) {
+      document.addEventListener('click', handler)
+      return () => document.removeEventListener('click', handler)
+    }
+  }, [when])
+}
